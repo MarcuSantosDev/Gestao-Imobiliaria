bb8b1ff78eec2c71cf2515c48a45a90e6bf3ba49
MarcusDev <marcus.xtreme03@gmail.com>
2026-06-06 01:23:33 -0300

    :sparkles: feat: Paginação por mais recentes ou antiga

--- apps/relatorios/views.py
+++ apps/relatorios/views.py
@@ -1,36 +1,61 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import TemplateView, View
+from django.views.generic import ListView, TemplateView, View
 
 from apps.imoveis.models import DemandaCliente, Imovel
 
 IMOVEIS_HISTORICO_STATUS = ('vendido', 'alugado', 'reservado')
 
 
+class HistoricoOrdenacaoMixin:
+    campo_data = None
+    paginate_by = 10
+
+    def get_ordem(self):
+        ordem = self.request.GET.get('ordem', 'recente')
+        return ordem if ordem in ('recente', 'antigo') else 'recente'
+
+    def ordenar_queryset(self, qs):
+        if self.get_ordem() == 'antigo':
+            return qs.order_by(self.campo_data, 'id')
+        return qs.order_by(f'-{self.campo_data}', '-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ordem = self.get_ordem()
+        context['ordem'] = ordem
+        context['ordem_proxima'] = 'antigo' if ordem == 'recente' else 'recente'
+        return context
+
+
 class HistoricoIndexView(TemplateView):
     template_name = 'historico/index.html'
 
 
-class HistoricoImoveisView(TemplateView):
+class HistoricoImoveisView(HistoricoOrdenacaoMixin, ListView):
+    model = Imovel
     template_name = 'historico/imoveis.html'
+    context_object_name = 'imoveis'
+    campo_data = 'finalizado_em'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['imoveis'] = Imovel.objects.filter(
+    def get_queryset(self):
+        qs = Imovel.objects.filter(
             status__in=IMOVEIS_HISTORICO_STATUS
-        ).select_related('corretor').order_by('-finalizado_em', '-id')
-        return context
+        ).select_related('corretor')
+        return self.ordenar_queryset(qs)
 
 
-class HistoricoDemandasView(TemplateView):
+class HistoricoDemandasView(HistoricoOrdenacaoMixin, ListView):
+    model = DemandaCliente
     template_name = 'historico/demandas.html'
+    context_object_name = 'demandas'
+    campo_data = 'atendida_em'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['demandas'] = DemandaCliente.objects.filter(
+    def get_queryset(self):
+        qs = DemandaCliente.objects.filter(
             status='atendida'
-        ).select_related('cliente').order_by('-atendida_em', '-id')
-        return context
+        ).select_related('cliente')
+        return self.ordenar_queryset(qs)
 
 
 class DemandaReabrirView(View):
